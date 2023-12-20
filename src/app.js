33d94c75a6fb13ed257a5acead0ea8908113ae92
Aitor <62630503+AitorMoyano@users.noreply.github.com>
2023-12-20 21:32:37 +0000

    feat: cartas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,47 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
+function newCard() {
+  let suits = ["♥", "♦", "♣", "♠"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  let top = document.querySelector("#top");
+  let botton = document.querySelector("#botton");
+  let number = document.querySelector("#number");
+
+  top.innerHTML = suits[indexSuits];
+  botton.innerHTML = suits[indexSuits];
+  number.innerHTML = numbers[indexNumbers];
+
+  let currentClass =
+    suits[indexSuits] == "♥" || suits[indexSuits] == "♦"
+      ? "text-danger"
+      : "text-dark";
+  top.className = currentClass;
+  botton.className = currentClass;
+  number.className = currentClass;
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
